d5f67cb267ea5ac94bfb94c95ef0a00f981af9c3
Douangmala Hugo <douangmala.hugo@gmail.com>
2018-07-02 11:41:35 +0200

    test2

---
+++ binomiaux.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
+
+
+/*
+Fonction qui prend en argument 2 entiers n et p.
+Elle permet de calculer le coefficient binomial b(n, p).
+Renvoie le résultat de b(n, p).
+*/
+int b(int n, int p){
+	if (0 >= n && 0 >= p){ /* On arrête le programme récursif lorsque n est inférieur ou égal à 0 ou que p soit inférieur ou égal à 0 */
+		return 1;
+	}
+	if (n == p || 0 == p){ /* Si n a la même valeur que o ou que p est nul on renvoie directement n */
+		return 1;
+	}
+	return b(n - 1, p) + b(n - 1, p - 1); 
+}
+
+int main(void){
+	int n;
+	int p;
+	printf("Veuillez saisir un entier n:\n");
+	scanf("%d", &n);
+	printf("Veuillez saisir un entier p inférieur à n:\n");
+	scanf("%d", &p);
+	if (p > n){
+		printf("Impossible, p est supérieur à n\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("b(%d, %d) = %d\n", n, p, b(n, p));
+	return 0;
+}
